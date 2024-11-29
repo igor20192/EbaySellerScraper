5a606ba69621937605ec57076e56da38ee39fb1d
igor20192 <igor.udovenko2012@meta.ua>
2024-11-29 23:36:03 +0200

    scraper.py  breaking code into functions

--- scraper.py
+++ scraper.py
@@ -4,7 +4,8 @@ import itertools
 import logging
 import re
 from typing import Dict, List
-from playwright.async_api import async_playwright, Page
+import playwright
+from playwright.async_api import async_playwright, Page, Browser
 from openpyxl import Workbook
 import pdb
 
@@ -34,11 +35,12 @@ TITLE_TABLES = [
     "бренд ",
     "кондиция товара",
 ]
+CONCURRENT_TASK_LIMIT = 4
 
 
 async def split_list_by_delimiter(lst, delimiter):
     """Split a list by separator."""
-    logging.info("Разделение списка по разделителю.")
+    logging.info("Разделение списка по разделителю.", exc_info=True)
     result = []
     current_sublist = []
     for item in lst:
@@ -56,12 +58,13 @@ async def split_list_by_delimiter(lst, delimiter):
 
 async def find_buttons(page, selector, level):
     """Search for buttons by a given selector."""
-    logging.info(f"Поиск кнопок на уровне {level}.")
+    logging.info(f"Поиск кнопок на уровне {level}.", exc_info=True)
     buttons = await page.query_selector_all(selector)
 
     if not buttons:
         logging.info(
-            f"Кнопки не найдены. Ожидание и повторный поиск на уровне {level}."
+            f"Кнопки не найдены. Ожидание и повторный поиск на уровне {level}.",
+            exc_info=True,
         )
         await asyncio.sleep(3)
         buttons = await page.query_selector_all(selector)
@@ -75,16 +78,17 @@ async def find_buttons(page, selector, level):
 async def select_option(page, button, value, level):
     """Select an option from the drop-down list."""
     try:
-        logging.info(f"Получение значения кнопки на уровне {level}.")
+        logging.info(f"Получение значения кнопки на уровне {level}.", exc_info=True)
         button_value = await button.get_attribute("value")
 
         if button_value and button_value.strip() == value.strip():
             logging.info(
-                f"Кнопка на уровне {level} уже содержит выбранное значение '{value}'. Пропускаем."
+                f"Кнопка на уровне {level} уже содержит выбранное значение '{value}'. Пропускаем.",
+                exc_info=True,
             )
             return
 
-        logging.info(f"Нажимаем кнопку на уровне {level}.")
+        logging.info(f"Нажимаем кнопку на уровне {level}.", exc_info=True)
         await button.click()
 
         # We are waiting for the drop-down list to appear
@@ -95,7 +99,7 @@ async def select_option(page, button, value, level):
             timeout=60000,
         )
 
-        logging.info(f"Выбираем опцию '{value}' из выпадающего списка.")
+        logging.info(f"Выбираем опцию '{value}' из выпадающего списка.", exc_info=True)
         option = await page.query_selector(f'div[role="option"]:has-text("{value}")')
 
         if option is None:
@@ -103,7 +107,9 @@ async def select_option(page, button, value, level):
             raise ValueError(f"Опция '{value}' не найдена на уровне {level}.")
 
         await option.click()
-        logging.info(f"Опция '{value}' успешно выбрана на уровне {level}.")
+        logging.info(
+            f"Опция '{value}' успешно выбрана на уровне {level}.", exc_info=True
+        )
 
     except Exception as e:
         logging.error(
@@ -116,13 +122,13 @@ async def select_option(page, button, value, level):
 async def get_price(page):
     """Get the price of the product."""
     try:
-        logging.info("Получение цены товара.")
+        logging.info("Получение цены товара.", exc_info=True)
         price_element = page.locator(
             'div[data-testid="x-price-primary"] span.ux-textspans'
         )
         await price_element.wait_for(state="visible", timeout=10000)
         price_text = await price_element.inner_text()
-        logging.info(f"Цена успешно получена: {price_text}")
+        logging.info(f"Цена успешно получена: {price_text}", exc_info=True)
         return price_text
     except Exception as e:
         logging.error(f"Ошибка при получении цены: {str(e)}", exc_info=True)
@@ -131,7 +137,7 @@ async def get_price(page):
 
 async def select_variant(page, variants):
     """Select product variants."""
-    logging.info("Выбор вариантов товара.")
+    logging.info("Выбор вариантов товара.", exc_info=True)
     DROPDOWN_BUTTON_SELECTOR = "button.listbox-button__control"
 
     for i, value in enumerate(variants):
@@ -172,7 +178,9 @@ async def extract_image_urls(page: Page) -> List[str]:
     """Extract up to 10 image URLs from a product page."""
     images = await page.query_selector_all("button.ux-image-grid-item img")
     image_urls = [await img.get_attribute("src") for img in images if img]
-    return image_urls[:10] + ["N/A"] * (10 - len(image_urls))
+    if len(image_urls) < 10:
+        image_urls[:10] + ["N/A"] * (10 - len(image_urls))
+    return image_urls[:10]
 
 
 async def get_listbox_values(page: Page):
@@ -191,7 +199,7 @@ async def get_listbox_values(page: Page):
 
 async def get_variant_values(item_page: Page):
     """Get product variants."""
-    logging.info("Получение вариантов товара.")
+    logging.info("Получение вариантов товара.", exc_info=True)
     try:
         listbox_values = await item_page.query_selector_all(".listbox__value")
         return [await element.inner_text() for element in listbox_values]
@@ -224,9 +232,11 @@ async def add_to_sheet(sheet: List[List[str]], product_data: Dict) -> None:
         logging.error(f"Ошибка при добавлении данных в лист: {e}", exc_info=True)
 
 
-async def process_variants(item_page: Page, product_data: dict, sheet: list):
+async def process_variants(
+    item_page: Page, product_data: dict[str, tuple[str]], sheet: list
+):
     """Processes product variants and writes the results to Excel."""
-    logging.info("Обработка вариантов товара.")
+    logging.info("Обработка вариантов товара.", exc_info=True)
     try:
         variant_values = await get_variant_values(item_page)
         variant_values = await split_list_by_delimiter(variant_values, "Select")
@@ -234,12 +244,16 @@ async def process_variants(item_page: Page, product_data: dict, sheet: list):
             for combo in itertools.product(*variant_values):
                 variant_data = product_data.copy()
                 variant_data["title"] = f"{product_data['title']} : {combo}"
-                price_variant = await select_variant(item_page, combo)
-                if price_variant:
-                    variant_data["price"] = price_variant
+                pattern = r"\$\d+\.\d+\s+to\s+\$\d+\.\d+"
+                if re.findall(pattern, variant_data["price"][0]):
+                    price_variant = await select_variant(item_page, combo)
+                    if price_variant:
+                        variant_data["price"] = price_variant
 
                 await add_to_sheet(sheet, variant_data)
-                logging.info(f"Вариант {variant_data['title']} обработан.")
+                logging.info(
+                    f"Вариант {variant_data['title']} обработан.", exc_info=True
+                )
         else:
             await add_to_sheet(sheet, product_data)
 
@@ -248,7 +262,7 @@ async def process_variants(item_page: Page, product_data: dict, sheet: list):
 
 
 async def process_product_variants(
-    item_page: Page, product_data: dict, sheet: Workbook
+    item_page: Page, product_data: dict, sheet: List[str]
 ):
     """
     Processes product variants and writes results to Excel.
@@ -268,24 +282,119 @@ async def process_product_variants(
         await item_page.close()
 
 
+async def get_product_data(
+    browser: Browser, page: Page, seller_url: str, sheet: list
+) -> None:
+    """Extracts product data from a product page."""
+    logging.info("Извлечение данных о продукте.", exc_info=True)
+    item_url = await page.query_selector("a.s-item__link")
+    item_url_href = await item_url.get_attribute("href") if item_url else "N/A"
+
+    title = (await extract_text(await page.query_selector(".s-item__title")),)
+    price = (await extract_text(await page.query_selector("span.s-item__price")),)
+    context = await browser.new_context(locale="en-US")
+
+    new_page = await context.new_page()
+    await new_page.goto(item_url_href, wait_until="domcontentloaded")
+
+    category = await extract_text(
+        await new_page.query_selector("ul li a.seo-breadcrumb-text span")
+    )
+    image_urls = await extract_image_urls(new_page)
+    quantity = await extract_text(
+        await new_page.query_selector("#qtyAvailability .ux-textspans--SECONDARY")
+    )
+    condition = await extract_text(
+        await new_page.query_selector(".x-item-condition-text .ux-textspans")
+    )
+    brand = await extract_text(
+        await new_page.query_selector(
+            "dl[data-testid='ux-labels-values'].ux-labels-values--brand dd span.ux-textspans"
+        )
+    )
+    product_data = {
+        "title": title,
+        "price": price,
+        "category": category,
+        "image_urls": image_urls,
+        "item_url_href": item_url_href,
+        "seller_url": seller_url,
+        "quantity": quantity,
+        "brand": brand,
+        "condition": condition,
+    }
+    await process_product_variants(new_page, product_data, sheet)
+
+
+async def process_products_in_batches(
+    browser: Browser, items, seller_urls: str, sheet: List[str]
+) -> None:
+    """Processes products in batches."""
+    logging.info("Обработка продуктов в пакетах...", exc_info=True)
+    semaphore = asyncio.Semaphore(CONCURRENT_TASK_LIMIT)
+
+    async def process_with_semaphore(browser, item, seller_urls, sheet):
+        async with semaphore:
+            await get_product_data(browser, item, seller_urls, sheet)
+
+    tasks = [
+        process_with_semaphore(browser, item, seller_urls, sheet) for item in items
+    ]
+    await asyncio.gather(*tasks)
+    logging.info("Обработка продуктов в пакетах завершена.", exc_info=True)
+
+
+async def safe_goto(page: Page, url, retries=3):
+    """
+    Safely navigates to a URL, handling network errors.
+
+    :param page: Playwright page object
+    :param url: URL to navigate to
+    :param retries: Number of retries for network errors
+    :return: None
+    """
+    logging.info(f"Navigating to {url}", exc_info=True)
+    for attempt in range(retries):
+        try:
+            await page.goto(url, wait_until="domcontentloaded", timeout=60000)
+            logging.info(f"Successfully navigated to {url}")
+            return  # Exit if successful
+        except playwright._impl._errors.Error as e:
+            if "net::ERR_NETWORK_CHANGED" in str(e):
+                logging.error(
+                    f"Network error encountered. Retry {attempt + 1} of {retries}",
+                    exc_info=True,
+                )
+                await asyncio.sleep(3)  # Wait before retrying
+            else:
+                logging.error(
+                    f"Unexpected error navigating to {url}: {e}", exc_info=True
+                )
+                raise  # Rethrow non-network errors
+    logging.error(
+        f"Failed to navigate to {url} after {retries} attempts", exc_info=True
+    )
+    raise RuntimeError(f"Failed to load {url} after {retries} attempts")
+
+
 async def parse_ebay_seller(seller_url):
     """
     Scrapes product data from an eBay seller's page and saves it to an Excel file.
 
     :param seller_url: URL of the eBay seller's page
     """
-    logging.info("Starting eBay seller scraping...")
+    logging.info("Starting eBay seller scraping...", exc_info=True)
 
     try:
         # Launch the Playwright browser
         async with async_playwright() as p:
             browser = await p.chromium.launch(headless=True)
-            logging.info("Browser launched in headless mode.")
+            logging.info("Browser launched in headless mode.", exc_info=True)
             page = await browser.new_page(locale="en-US")
 
             # Open the seller's URL
             await page.goto(seller_url)
-            logging.info(f"Navigated to {seller_url}")
+            logging.info(f"Navigated to {seller_url}", exc_info=True)
 
             # Scroll the page to load all items (if applicable)
             await scroll_to_load(page)
@@ -293,9 +402,15 @@ async def parse_ebay_seller(seller_url):
             # Select product elements
             await page.wait_for_selector("ul.srp-results.srp-list")
             items = await page.query_selector_all("ul.srp-results.srp-list li.s-item")
+            logging.info(f"Found {len(items)} items on the page.", exc_info=True)
 
-            logging.info(f"Found {len(items)} items on the page.")
-
+            pagination_links = await page.eval_on_selector_all(
+                ".pagination__items a.pagination__item",
+                "elements => elements.map(e => e.href)",
+            )
+            logging.info(
+                f"Found {len(pagination_links)} pagination links.", exc_info=True
+            )
             # Create an Excel workbook and add headers
             workbook = Workbook()
             sheet = workbook.active
@@ -303,83 +418,36 @@ async def parse_ebay_seller(seller_url):
             sheet.append(TITLE_TABLES)
 
             # Extract data for each product
-            for item in items:
+            for pagination_link in pagination_links:
+                logging.info(
+                    f"Processing pagination link: {pagination_link}", exc_info=True
+                )
+                new_page = await browser.new_page(locale="en-US")
+                logging.info("New page created.", exc_info=True)
+                new_page.context.new_page(locale="en-US")
+                # await page.goto(pagination_link, wait_until="domcontentloaded")
+                await safe_goto(new_page, pagination_link)
+                if page.is_closed():
+                    logging.info("Page was unexpectedly closed. Recreating...")
+                    new_page = await browser.new_page(locale="en-US")
+                await scroll_to_load(new_page)
+                items = await page.query_selector_all(
+                    "ul.srp-results.srp-list li.s-item"
+                )
+                logging.info(f"Found {len(items)} items on the page.", exc_info=True)
                 try:
-                    # Extract product details
-                    title = await extract_text(
-                        await item.query_selector(".s-item__title")
-                    )
-                    price = await extract_text(
-                        await item.query_selector("span.s-item__price")
-                    )
-                    # Extract product link
-                    item_url = await item.query_selector("a.s-item__link")
-                    item_url_href = (
-                        await item_url.get_attribute("href") if item_url else "N/A"
-                    )
-                    if not item_url_href:
-                        logging.warning("Item URL not found, skipping this item.")
-                        continue
-
-                    context = await browser.new_context()
-                    item_page = await context.new_page()
-                    await item_page.goto(item_url_href, wait_until="domcontentloaded")
-
-                    category = await extract_text(
-                        await item_page.query_selector(
-                            "ul li a.seo-breadcrumb-text span"
-                        )
-                    )
-
-                    # Extract available quantity text
-
-                    quantity = await extract_text(
-                        await item_page.query_selector(
-                            "#qtyAvailability .ux-textspans--SECONDARY"
-                        )
-                    )
-
-                    # Replace your existing condition element code with:
-                    condition = await extract_text(
-                        await item_page.query_selector(
-                            ".x-item-condition-text .ux-textspans"
-                        )
-                    )
-
-                    # Extract brand text
-                    brand = await extract_text(
-                        await item_page.query_selector(
-                            "dl[data-testid='ux-labels-values'].ux-labels-values--brand dd span.ux-textspans"
-                        )
-                    )
-
-                    # Extract all images
-                    image_urls = await extract_image_urls(item_page)
-
-                    product_data = {
-                        "title": title,
-                        "price": price,
-                        "category": category,
-                        "image_urls": image_urls,
-                        "item_url_href": item_url_href,
-                        "seller_url": seller_url,
-                        "quantity": quantity,
-                        "brand": brand,
-                        "condition": condition,
-                    }
-                    await process_product_variants(item_page, product_data, sheet)
-
+                    await process_products_in_batches(browser, items, seller_url, sheet)
                 except Exception as e:
-                    logging.error(f"Error processing item: {e}", exc_info=True)
+                    logging.error(f"Error processing products: {e}", exc_info=True)
 
             # Save the Excel file
             output_file = "ebay_seller_data_playwright.xlsx"
             workbook.save(output_file)
-            logging.info(f"Data successfully saved to {output_file}")
+            logging.info(f"Data successfully saved to {output_file}", exc_info=True)
 
             # Close the browser
             await browser.close()
-            logging.info("Browser closed.")
+            logging.info("Browser closed.", exc_info=True)
     except Exception as e:
         logging.critical(f"An unexpected error occurred: {e}", exc_info=True)
 
